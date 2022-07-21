8857f94a83172fcd8ec8e97ca5c2697c6b7c7973
Carball0ULE <alex_1280@hotmail.es>
2022-07-22 01:27:44 +0200

    Modified styles

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,7 +9,7 @@ import InsertLinkIcon from '@mui/icons-material/InsertLink';
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-20 m-0
-                        flex flex-col bg-slate-800 text-gray-200">
+                        flex flex-col bg-slate-800">
             <SideBarIcon icon={<GitHubIcon sx={{ fontSize: 42 }} />} text={"GitHub"}/>
             <SideBarIcon icon={<TwitterIcon sx={{ fontSize: 42 }} />} text={"Twitter"}/>
             <SideBarIcon icon={<LinkedInIcon sx={{ fontSize: 40 }} />} text={"LinkedIn"}/>
--- src/components/SideBarIcon.js
+++ src/components/SideBarIcon.js
@@ -1,9 +1,10 @@
 import IconButton from '@mui/material/IconButton';
+import { blue } from '@mui/material/colors';
 
 const SideBarIcon = ({icon, text}) => {
     return (
         <div className="icon group">
-            <IconButton>
+            <IconButton sx={{ color: blue[200] }}>
                 {icon}
             </IconButton> 
             <p className="icon-text group-hover:scale-100">
