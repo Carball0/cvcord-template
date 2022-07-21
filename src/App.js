414882b8bc59b54c4319dbea9a54deaebe0fe487
Carball0ULE <alex_1280@hotmail.es>
2022-07-21 20:28:33 +0200

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 align="center">I'm empty yet, but not for long :3</h1>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',    //Mode Just In Time, faster builds
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
